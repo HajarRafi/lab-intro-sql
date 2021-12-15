3238c18583b20ffb913bc192755070f2bc35d6df
Hajar Rafibayli <hajar.rafibayli@gmail.com>
2021-12-15 09:18:28 +0100

    intro Sql

---
+++ Sakila.sql
@@ -0,0 +1,6 @@
+select * from actor, film, customer;
+select distinct title from film;
+select distinct name as language from language;
+select count(store_id) from store;
+select count(staff_id) from staff;
+select first_name from staff;
